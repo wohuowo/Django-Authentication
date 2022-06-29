56d2e9ed1765e149c09116c0b034d1f68f201577
Tony Obriku <tonyobriku@gmail.com>
2022-06-29 03:38:24 +0100

    shows how to make customform

---
+++ authapp/forms.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True, help_text="")
+    first_name = forms.CharField(required=True,)
+    class Meta:
+        model = User
+        fields = ('first_name', 'email', 'username', 'password1', 'password2')
+
