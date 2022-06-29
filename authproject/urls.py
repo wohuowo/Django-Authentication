63ef92584ca962f6f201982583a82de892635a2c
Tony Obriku <tonyobriku@gmail.com>
2022-06-29 01:46:56 +0100

    new url patterns for different patterns added

--- authproject/urls.py
+++ authproject/urls.py
@@ -18,4 +18,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', views.home, name = 'home'),
+    path('signin/', views.signin, name = 'signin'),
+    path('signup/', views.signup, name = 'signup'),
+    path('signout/', views.signout, name = 'signout'),
 ]
