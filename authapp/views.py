ce598dfdd28f1c95d40a0af09fdf2694895fad45
Tony Obriku <tonyobriku@gmail.com>
2022-06-29 02:49:04 +0100

    authentification login written here

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+#renders first parameter is a request
+@login_required
+def home(request):
+    user = request.user.username
+    return render(request, 'ZuriTeam/home.html', {'username': user})
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)#user creation form is default django form
+        if form.is_valid():
+            form.save
+            username = form.cleaned_data.get('username')#this is a dictionary
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+             form = UserCreationForm()
+        return render(request, 'ZuriTeam/register.html', {'form':form})    
+    else:
+        form = UserCreationForm()
+        return render(request, 'ZuriTeam/register.html', {'form':form})        
+    
+def signin(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)#user creation form is default django form
+        if form.is_valid():
+            form.save
+            username = form.cleaned_data.get('username')#this is a dictionary
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        else:
+             form = UserCreationForm()
+        return render(request, 'ZuriTeam/index.html', {'form' : form})    
+    else:
+        form = UserCreationForm()
+        return render(request, 'ZuriTeam/index.html', {'form' : form})        
+    
+def signout(request):
+    logout(request)
+    return redirect('signin')
